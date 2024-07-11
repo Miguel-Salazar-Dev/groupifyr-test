7b16015f40b3fb48d46ddc201892327a0c810c55
Miguel Salazar <miguelsalazartomey@gmail.com>
2024-07-10 20:09:05 -0400

    Google Auth Implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
-export default function Home () {
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { AuthButtonServer } from './components/auth-button-server'
+
+export default async function Home () {
+  const supabase = createServerComponentClient({ cookies })
+  const { data: messages } = await supabase.from('messages').select()
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hola Groupifyr 👋
+      <AuthButtonServer />
+      <pre>
+        {
+          JSON.stringify(messages, null, 2)
+        }
+      </pre>
+
     </main>
   )
 }
